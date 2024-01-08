981e7eb46f8a823c54dca2b4e1b597e9735042a6
Daniel Nunes <danyribeironunes15@gmail.com>
2024-01-08 21:28:19 +0000

    refactor: Tailwind config file is now using TypeScript

--- svelte/tailwind.config.ts
+++ svelte/tailwind.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { Config } from "tailwindcss";
+
+export default {
     content: [
         "./src/**/*"
     ],
@@ -9,6 +11,9 @@ module.exports = {
                 "125": "31.25rem"
             },
             colors: {
+                primary: "theme(colors.indigo.600)",
+                "primary-dark": "theme(colors.indigo.800)",
+                "primary-light": "theme(colors.indigo.500)",
                 foreground: "var(--color-foreground)",
                 background: "var(--color-background)",
                 secondary: "var(--color-secondary)",
@@ -21,4 +26,4 @@ module.exports = {
         }
     },
     plugins: []
-}
+} satisfies Config
