ae85e6f871beca26dfea62cd010126fd92cbfea3
Daniel Nunes <danyribeironunes15@gmail.com>
2024-01-23 22:45:12 +0000

    feat: ComboBox can now be reversed to open upwards

--- svelte/src/lib/components/ComboBox.svelte
+++ svelte/src/lib/components/ComboBox.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import { createEventDispatcher } from "svelte";
     import { slide } from "svelte/transition";
-    import { quintOut } from "svelte/easing";
+    import { transition } from "$lib/stores/transitionStore";
     import Icon from "./Icon.svelte";
 
     export let className: string = "";
     export let items: string[];
     export let selected: number = 0;
     export let listClassName: string = "";
+    export let listReverse = false;
 
-    let open = false;
+    let open = false, closedCss = !listReverse ? "" : "-rotate-180", openCss = !listReverse ? "-rotate-180" : "rotate-0";
     const dispatch = createEventDispatcher();
     $: selectedItem = items[selected];
 
@@ -23,13 +24,13 @@
 <button class={`block relative rounded-md z-10 ${className}`} role="combobox" aria-controls="comboboxItems" aria-expanded={open} on:click={() => open = !open}>
     <div class="px-2 py-1.5 flex justify-between items-center bg-tertiary text-left rounded-md">
         <span class="text-sm">{selectedItem}</span>
-        <Icon name="chevron" className={`w-5 h-5 ml-2 fill-current transition-transform duration-500 ${open ? "-rotate-180" : ""}`} />
+        <Icon name="chevron" className={`w-5 h-5 ml-2 fill-current transition-transform duration-500 ease-quint-out ${!open ? closedCss : openCss}`} />
     </div>
     {#if open}
-        <div class="w-full absolute top-6 bg-tertiary rounded-b-md overflow-hidden -z-10" transition:slide={{ duration: 500, easing: quintOut }}>
+        <div class={`w-full absolute ${!listReverse ? "top-6" : "bottom-6"} bg-tertiary rounded-md overflow-hidden -z-10`} transition:slide={$transition.comboFlow}>
             <div id="comboboxItems" class={`max-h-[7.5rem] flex flex-col bg-shade/5 overflow-y-scroll ${listClassName}`}>
                 {#each items as item, i}
-                    <button class={"px-2 py-1.5 text-left text-sm " + (i == 0 ? "pt-3.5" : "")} on:click={() => itemSelected(i)}>{item}</button>
+                    <button class={`px-2 py-1.5 text-left text-sm ${i == 0 && !listReverse ? "pt-3.5" : (i == items.length - 1 && listReverse ? "pb-3.5" : "")}`} on:click={() => itemSelected(i)}>{item}</button>
                 {/each}
             </div>
         </div>
