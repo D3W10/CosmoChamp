1b6b812bf645e5e2fbbccd596cbe70904db66bea
Daniel Nunes <danyribeironunes15@gmail.com>
2024-01-25 16:23:50 +0000

    feat: Extend reduce motion across other elements

--- svelte/src/lib/pages/Game.svelte
+++ svelte/src/lib/pages/Game.svelte
@@ -48,28 +48,28 @@
             </div>
             <div class="w-[115%] h-1/2 flex items-center space-x-0.5">
                 <div class="w-full h-full flex justify-center items-start pt-10 overflow-hidden -skew-x-[35deg]">
-                    <p class="mr-12 text-shade/20 text-4xl font-semibold skew-x-[35deg] whitespace-nowrap" in:fly={{ duration: 2000, x: -410, easing: quintOut, opacity: 1 }} out:fly={{ duration: 2000, x: 500, easing: quintIn, opacity: 1 }} on:introend={() => versus = false}>{$game?.host ? $settings?.playerName : $game?.opponent}</p>
+                    <p class="mr-12 text-shade/20 text-4xl font-semibold skew-x-[35deg] whitespace-nowrap" in:fly={$transition.nameFlyIn(true)} out:fly={$transition.nameFlyOut(false)} on:introend={() => versus = false}>{$game?.host ? $settings?.playerName : $game?.opponent}</p>
                 </div>
                 <div class="min-w-[1.5rem] max-w-[1.5rem] h-full flex justify-center items-center bg-shade/40 rounded-lg rotate-[35deg]" in:fade={{ duration: 1000 }} out:fade={{ duration: 1000, delay: 1500 }} on:outroend={() => start = true}>
                     <p class="text-7xl font-semibold rotate-[-35deg]">VS</p>
                     <p class="absolute text-7xl font-semibold rotate-[-35deg] drop-shadow-glow animate-pulse">VS</p>
                 </div>
                 <div class="w-full h-full flex justify-center items-end pb-10 overflow-hidden -skew-x-[35deg]">
-                    <p class="ml-12 text-shade/20 text-4xl font-semibold skew-x-[35deg] whitespace-nowrap" in:fly={{ duration: 2000, x: 410, easing: quintOut, opacity: 1 }} out:fly={{ duration: 2000, x: -500, easing: quintIn, opacity: 1 }}>{!$game?.host ? $settings?.playerName : $game?.opponent}</p>
+                    <p class="ml-12 text-shade/20 text-4xl font-semibold skew-x-[35deg] whitespace-nowrap" in:fly={$transition.nameFlyIn(false)} out:fly={$transition.nameFlyOut(true)}>{!$game?.host ? $settings?.playerName : $game?.opponent}</p>
                 </div>
             </div>
         </div>
     {/if}
     {#if start}
         <div class="w-full h-full flex flex-col relative">
-        <div class="px-6 flex justify-between items-start">
-            <div class="flex -mt-20">
+            <div class="px-6 flex justify-between items-start">
+                <div class="flex -mt-20">
                     {#each Array(7) as _, i}
                         {#if !oSendState[i]}
                             <img class={`${i != 0 ? "-ml-10" : ""} enemy-card ${opponentHover == 6 - i ? "translate-y-5" : ""}`} src="./cards/back.png" alt="Enemy Card" style={`z-index: ${i};`} in:fly|global={{ duration: 800, y: -100, delay: 600 - i * 100, easing: backOut }} out:receive={{ key: "oCard" }} />
                         {/if}
                     {/each}
-            </div>
+                </div>
                 <div class="mt-6 flex space-x-6" in:fly={{ duration: 800, x: 300 }}>
                     <span class="flex text-shade/50">0<img class="w-6 mx-0.5" src="./point.png" alt="Cosmo Points" title="Cosmo Points" />points</span>
                 <span>{$game?.opponent}</span>
@@ -96,24 +96,24 @@
                             {/if}
                         {/each}
                     </div>
-            </div>
-            <div class="sides flex flex-col justify-center items-center space-y-3" in:fade={{ delay: 300, duration: 800 }}>
-                <div class="flex space-x-3">
-                    <img class="w-16 h-16 opacity-10" src="./elements/energy.png" alt="Energy Element" />
-                    <img class="w-16 h-16 opacity-10" src="./elements/wind.png" alt="Wind Element" />
                 </div>
-                <div class="flex space-x-3">
-                    <img class="w-16 h-16 opacity-10" src="./elements/nature.png" alt="Nature Element" />
-                    <img class="w-16 h-16 opacity-10" src="./logo.png" alt="Space Element" />
+                <div class="sides flex flex-col justify-center items-center space-y-3" in:fade={{ delay: 300, duration: 800 }}>
+                    <div class="flex space-x-3">
+                        <img class="w-16 h-16 opacity-10" src="./elements/energy.png" alt="Energy Element" />
+                        <img class="w-16 h-16 opacity-10" src="./elements/wind.png" alt="Wind Element" />
+                    </div>
+                    <div class="flex space-x-3">
+                        <img class="w-16 h-16 opacity-10" src="./elements/nature.png" alt="Nature Element" />
+                        <img class="w-16 h-16 opacity-10" src="./logo.png" alt="Space Element" />
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="px-6 flex justify-between items-end">
+            <div class="px-6 flex justify-between items-end">
                 <div class="mb-6 flex space-x-6" in:fly={{ duration: 800, x: -300 }}>
-                <span>{$settings?.playerName}</span>
+                    <span>{$settings?.playerName}</span>
                     <span class="flex text-shade/50">0<img class="w-6 mx-0.5" src="./point.png" alt="Cosmo Points" title="Cosmo Points" />points</span>
-            </div>
-            <div class="flex -mb-20">
+                </div>
+                <div class="flex -mb-20">
                     {#each cards as card, i}
                         {#if !pSendState[i]}
                             <button class={`${i != 0 ? "-ml-10 " : ""} player-card hover:-translate-y-5`} style={`z-index: ${i};`} in:fly|global={{ duration: 800, y: 150, delay: i * 100, easing: backOut }} out:receive={{ key: "pCard" }} on:click={() => { pSendState[i] = true; $app?.sendMessage(`SELECT ${i}`) }} on:pointerenter={() => $app?.sendMessage(`HOVER ${i}`)} on:pointerleave={checkHoverState}>
--- svelte/src/lib/stores/transitionStore.ts
+++ svelte/src/lib/stores/transitionStore.ts
@@ -1,15 +1,17 @@
 import { derived } from "svelte/store";
 import { page } from "./pageStore";
 import { settings } from "./settingsStore";
-import { cubicIn, cubicOut, quintOut } from "svelte/easing";
+import { cubicIn, cubicOut, quintIn, quintOut } from "svelte/easing";
 
 export const transition = derived([page, settings], ($values) => {
-    let duration = !$values[1]?.reduceMotion ? 500 : 200;
-    let offset = (!$values[1]?.reduceMotion ? 200 : 100) * (!$values[0].back ? -1 : 1);
+    let duration = !$values[1]?.reduceMotion ? 500 : 300;
+    let offset = (!$values[1]?.reduceMotion ? 200 : 50) * (!$values[0].back ? -1 : 1);
 
     return {
         pageIn: { duration: duration, delay: duration, x: offset * -1, easing: cubicOut },
         pageOut: { duration: duration, x: offset, easing: cubicIn },
-        comboFlow: { duration: 500, easing: quintOut }
+        comboFlow: { duration: 500, easing: quintOut },
+        nameFlyIn: (rev: boolean) => { return { duration: 2000, x: (!$values[1]?.reduceMotion ? 410 : 205) * (!rev ? 1 : -1), easing: quintOut, opacity: (!$values[1]?.reduceMotion ? 1 : 0) }},
+        nameFlyOut: (rev: boolean) => { return { duration: 2000, x: (!$values[1]?.reduceMotion ? 500 : 250) * (!rev ? 1 : -1), easing: quintIn, opacity: (!$values[1]?.reduceMotion ? 1 : 0) }}
     };
 });
