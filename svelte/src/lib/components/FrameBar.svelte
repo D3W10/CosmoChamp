e82e8524f50a30c3ebd69362b0cce48add3f4f8a
Daniel Nunes <danyribeironunes15@gmail.com>
2024-09-15 22:51:10 +0100

    fix: Fix weird spacing on frame bar on Windows

--- svelte/src/lib/components/FrameBar.svelte
+++ svelte/src/lib/components/FrameBar.svelte
@@ -28,13 +28,13 @@
 <div class="w-full h-10 p-2 flex justify-between drag">
     {#if $app}
         {@const platform = $app.getPlatform()}
-        <div class="w-32 flex items-center space-x-2 {platform == "darwin" ? "pl-[4.5rem]" : ""}">
+        <div class="w-1/2 flex items-center space-x-2 {platform == "darwin" ? "pl-[4.5rem]" : ""}">
             {#if platform != "darwin"}
                 <img class="h-5 ml-0.5" src="./logo.png" alt="{$info.name} Logo" />
             {/if}
             <span class="text-sm font-semibold">{$info.name}</span>
         </div>
-        <div class="w-32 flex items-center p-1 space-x-2">
+        <div class="w-1/2 flex justify-end items-center p-1 space-x-2">
             {#if platform != "darwin"}
                 <button class="w-4 h-4 flex justify-center items-center relative bg-tertiary rounded-full overflow-hidden before:w-0 before:h-0 before:absolute before:bg-amber-500 before:rounded-full before:transition-all before:duration-[400ms] before:ease-cubic-out hover:before:w-9 hover:before:h-9 focus-visible:outline-amber-500" on:click={$app.minimizeWindow}></button>
                 <button class="w-4 h-4 flex justify-center items-center relative bg-tertiary rounded-full overflow-hidden before:w-0 before:h-0 before:absolute before:bg-red-500 before:rounded-full before:transition-all before:duration-[400ms] before:ease-cubic-out hover:before:w-9 hover:before:h-9 focus-visible:outline-red-500" on:click={onRedButtonClick}></button>
